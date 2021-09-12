c27d2b2f8feb36666c12e8f2f32be27b27a725cf
Thiago Lisboa <atomic-feast@hotmail.com>
2021-09-12 19:35:54 -0300

    feat(*): created auto info completion path
    made by @VitorGouveia

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
-console.log(location.search.split("?=")[1]);
-import * as posts from "./json/posts.json"
+import { posts } from "./json/allPosts.js"
 
-console.log(posts);
+/* <h1 class="post-title" /> */
+const postLink = document.querySelector(".content-link")
+const postImg = document.querySelector(".banner-image")
+const postTitle = document.querySelector(".post-title")
+/**
+ * ?=title -> ["", "title"] -> "title"
+*/
+const urlPostName = location.search.split("?=")[1]
+
+posts.map(({ title, name, img  }) => {
+  if(urlPostName === name) {
+    postTitle.innerHTML = title
+
+    const { author, url, description } = img
+
+    postLink.setAttribute("href", author)
+    postImg.setAttribute("src", url)
+    postImg.setAttribute("alt", description)
+  }
+});
---
+++ js/json/allPosts.js
@@ -0,0 +1,14 @@
+export const posts = [
+    {   
+        title: "Apartamentos em São Paulo hoje estão na marca de preços exorbitantes",
+        name: "apartamentos-em-sao-paulo-hoje-estao-na-marca-de-precos-exorbitantes",
+        img: {
+            author: "https://www.pexels.com/pt-br/foto/bege-creme-tigela-bowl-7046004/",
+            url: "../../images/pexels-max-vakhtbovych.jpg",
+            description: "Apartamentos em São Paulo hoje estão na marca de preços exorbitantes"
+        }
+    },
+    {
+        name: "apartament",
+    },
+]
